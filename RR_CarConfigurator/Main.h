3b6ec126a14c2e1a129cc545e2799dcb3da158dc
Christoph <cheichler@uos.de>
2014-11-10 14:53:12 +0100

    Initial Commit

---
+++ RR_CarConfigurator/Main.h
@@ -0,0 +1,97 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <Windows.h>
+#include <iostream>
+#include <stdlib.h>
+#include <gl\glew.h>
+#include <gl\freeglut.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <iostream>
+#include <GL\glew.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_inverse.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <GL\freeglut.h>
+#include <GL\gl.h>
+#include <GL\glu.h>
+#include <sstream>
+
+
+#include <foreach.hpp>
+#include <boost\property_tree\ptree.hpp>
+#include <boost\property_tree\ptree_fwd.hpp>
+#include <boost\property_tree\json_parser.hpp>
+#include <boost\property_tree\json_parser.hpp>
+
+
+
+#include "Modell.h"
+#include "Util.h"
+
+
+
+using namespace std;
+using boost::property_tree::ptree;
+
+GLuint vaoId = 0;
+GLuint vboId = 0;
+GLuint iboId = 0;
+GLuint naoId = 0;
+GLuint nboId = 0;
+GLuint framebuffer;
+GLuint programID = 0;
+GLuint ATTR_POS;
+GLuint ATTR_NORMAL;
+
+//Kommunikation zwischen OpenGL und CUDA
+
+vector<int> vbo;
+vector<int> ibo;
+vector<int> nbo;
+vector<Modell*> part;
+
+int width = 800;
+int height = 600;
+float factor = 0.0004f;
+
+//Shader Stuff
+float translation_X = 0.0f;
+float translation_Y = 0.0f;
+float zoom = -4.0f;
+float rotationAngle_X = 18.0f;
+float rotationAngle_Y = 0.0f;
+
+bool vw = false;
+
+int partID;
+
+//User Input
+bool keyStates[256];
+bool keySpecialStates[246];
+bool lastState;
+
+int recentMouse_X;
+int recentMouse_Y;
+int mouseButton = 0;
+int mousePressed = 1;
+
+SYSTEMTIME st;
+WORD currentTimeMS;
+WORD lastTimeMS;
+
+FILE* f;
+
+
+//Cuda Zeugs
+unsigned char* devPtr;
+vector<unsigned char> yuv;
+unsigned char* d_yuv;
+size_t arraySize;
+
+int countFrame;
+
+#endif
---
+++ RR_CarConfigurator/Util.h
@@ -0,0 +1,20 @@
+/*
+ * Util.hpp
+ *
+ *  Created on: 31.07.2013
+ *      Author: christoph
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+std::string textFileRead(const char* filePath);
+
+
+#endif
